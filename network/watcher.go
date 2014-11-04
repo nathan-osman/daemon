09a63db15a941cf26ec0aead6bcd71345b467f64
Nathan Osman <nathan@quickmediasolutions.com>
2014-11-03 23:21:37 -0800

    Allowed for multiple listeners for network interface events.

--- network/watcher.go
+++ network/watcher.go
@@ -10,45 +10,62 @@ import (
 // Interval for refreshing the interfaces.
 const refreshDuration = 10 * time.Second
 
-// Monitor for the addition and removal of network interfaces.
+// Status update on an interface.
+type Status struct {
+	Status bool
+	Name   string
+}
+
+// Watcher for the addition and removal of network interfaces.
 type Watcher struct {
-	Added   chan string
-	Removed chan string
-	Stop    chan bool
-	names   set.Set
+	Stop      chan bool
+	listeners []chan Status
+	oldNames  set.Set
 }
 
 // Create a new watcher.
 func NewWatcher() *Watcher {
 	w := &Watcher{
-		Added:   make(chan string),
-		Removed: make(chan string),
-		Stop:    make(chan bool),
-		names:   set.NewSet(),
+		Stop:     make(chan bool, 1),
+		oldNames: set.NewSet(),
 	}
 	go w.watch()
 	return w
 }
 
+// Return a channel for receiving notifications.
+func (w *Watcher) Listen() chan Status {
+	c := make(chan Status)
+	w.listeners = append(w.listeners, c)
+	return c
+}
+
+// Send a message to all listeners.
+func (w *Watcher) notify(status Status) {
+	for _, c := range w.listeners {
+		c <- status
+	}
+}
+
 // Refresh the list of interfaces.
 func (w *Watcher) refresh() error {
-	names := set.NewSet()
+	newNames := set.NewSet()
 	ifis, err := net.Interfaces()
 	if err != nil {
 		return err
 	}
 	for _, ifi := range ifis {
 		if ifi.Flags&net.FlagUp != 0 && ifi.Flags&(net.FlagBroadcast|net.FlagMulticast) != 0 {
-			names.Add(ifi.Name)
+			newNames.Add(ifi.Name)
 		}
 	}
-	for name := range names.Difference(w.names).Iter() {
-		w.Added <- name.(string)
+	for name := range newNames.Difference(w.oldNames).Iter() {
+		w.notify(Status{true, name.(string)})
 	}
-	for name := range w.names.Difference(names).Iter() {
-		w.Removed <- name.(string)
+	for name := range w.oldNames.Difference(newNames).Iter() {
+		w.notify(Status{false, name.(string)})
 	}
-	w.names = names
+	w.oldNames = newNames
 	return nil
 }
 
